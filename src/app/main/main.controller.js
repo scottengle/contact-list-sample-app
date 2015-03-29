f073ad8a8d97ea6b1a9a009bb948c1f17f810a5d
Scott Engle <scottengle22@gmail.com>
2015-03-29 02:27:57 -0600

    Updated styles. Dynamically loading data from .json file. Updated member directory look and feel.

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,46 +1,29 @@
 'use strict';
 
 angular.module('sampleAssignment')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      {
-        'title': 'AngularJS',
-        'url': 'https://angularjs.org/',
-        'description': 'HTML enhanced for web apps!',
-        'logo': 'angular.png'
-      },
-      {
-        'title': 'BrowserSync',
-        'url': 'http://browsersync.io/',
-        'description': 'Time-saving synchronised browser testing.',
-        'logo': 'browsersync.png'
-      },
-      {
-        'title': 'GulpJS',
-        'url': 'http://gulpjs.com/',
-        'description': 'The streaming build system.',
-        'logo': 'gulp.png'
-      },
-      {
-        'title': 'Jasmine',
-        'url': 'http://jasmine.github.io/',
-        'description': 'Behavior-Driven JavaScript.',
-        'logo': 'jasmine.png'
-      },
-      {
-        'title': 'Karma',
-        'url': 'http://karma-runner.github.io/',
-        'description': 'Spectacular Test Runner for JavaScript.',
-        'logo': 'karma.png'
-      },
-      {
-        'title': 'Protractor',
-        'url': 'https://github.com/angular/protractor',
-        'description': 'End to end test framework for AngularJS applications built on top of WebDriverJS.',
-        'logo': 'protractor.png'
-      }
-    ];
-    angular.forEach($scope.awesomeThings, function(awesomeThing) {
-      awesomeThing.rank = Math.random();
-    });
+  .controller('MainCtrl', function ($scope, $http) {
+
+    $http.get('/data/sample-data.json')
+         .then(function(resp) {
+          $scope.members = resp.data;
+         })
+         .then(function() {
+          angular.forEach($scope.members, function(member) {
+            var phoneArr = member.phone.split('-');
+            member.formatted_phone = ''.format('{0} ({1}) {2}-{3}', phoneArr[0], phoneArr[1], phoneArr[2], phoneArr[3]);
+            member.full_name = ''.format('{0} {1}', member.first_name, member.last_name);
+          });
+         });
+
   });
+
+String.prototype.format = function() {
+  var str = arguments[0];
+
+  for (var i=1; i<arguments.length; i++) {
+    var regEx = new RegExp("\\{" + (i-1) + "\\}", "gm");
+    str = str.replace(regEx, arguments[i]);
+  }
+
+  return str;
+}
