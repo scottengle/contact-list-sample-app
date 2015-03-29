9e21e33479444db006c674da59d036294092bc01
Scott Engle <scottengle22@gmail.com>
2015-03-29 04:31:30 -0600

    Added filters. Added styles. Beginning work on pagination.

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -8,12 +8,20 @@ angular.module('sampleAssignment')
           $scope.members = resp.data;
          })
          .then(function() {
+          $scope.member_states = [];
           angular.forEach($scope.members, function(member) {
             var phoneArr = member.phone.split('-');
             member.formatted_phone = ''.format('{0} ({1}) {2}-{3}', phoneArr[0], phoneArr[1], phoneArr[2], phoneArr[3]);
             member.full_name = ''.format('{0} {1}', member.first_name, member.last_name);
+            if($scope.member_states.indexOf(member.state) === -1) {
+              $scope.member_states.push(member.state);
+            }
           });
+          $scope.member_states.sort();
          });
+
+    $scope.pager = {};
+    $scope.pager.pager_options = [10, 20, 50];
 
   });
 
