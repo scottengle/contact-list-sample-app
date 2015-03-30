600a22eefef51771fa5a7d34abbd0b38aeead43f
Scott Engle <scottengle22@gmail.com>
2015-03-29 21:18:52 -0600

    Fixed interaction between search filter and state dropdown. Added hover effects for buttons and links per the style guide. Added a clear filters button. Added pagination.

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -21,6 +21,17 @@ function formatFullName(firstName, lastName) {
   return format('{0} {1}', firstName, lastName);
 }
 
+function getStatesByMembers(members) {
+  var states = [];
+  angular.forEach(members, function(member) {
+    if(states.indexOf(member.state) === -1) {
+      states.push(member.state);
+    }
+  });
+  states.sort();
+  return states;
+}
+
 angular.module('aaae')
   .controller('MainCtrl', ['$scope', '$http', '$timeout', function ($scope, $http, $timeout) {
 
@@ -34,36 +45,59 @@ angular.module('aaae')
         $scope.members = resp.data;
       })
       .then(function() {
-        angular.forEach($scope.members, function(member) {
+
+        angular.forEach($scope.members, function(member, idx) {
+
           member.formattedPhone = formatPhone(member.phone);
+
           member.fullName = formatFullName(member.first_name, member.last_name);
-          if($scope.memberStates.indexOf(member.state) === -1) {
-            $scope.memberStates.push(member.state);
-          }
+
+          member.id = idx;
+
         });
-        $scope.memberStates.sort();
+
+        $scope.memberStates = getStatesByMembers($scope.members);
       })
       .then(function() {
-        $scope.pager.numPages = Math.ceil($scope.members.length/$scope.pager.perPage);        
+        $scope.pager.numPages = Math.ceil($scope.members.length/$scope.pager.perPage);
       });
 
     $scope.paginate = function() {
       $timeout(function() {
-        $scope.pager.numPages = Math.ceil($scope.filterMembers.length/$scope.pager.perPage);
+        $scope.pager.numPages = ($scope.filterMembers.length === 0) ? 1 : Math.ceil($scope.filterMembers.length/$scope.pager.perPage);
         $scope.currentPage = 1;
       }, 10);
     };
 
-    $scope.pageUp = function() {
-      if($scope.currentPage < $scope.pager.numPages) {
-        $scope.currentPage++;
-      }
+    $scope.paginateAndFilterStates = function() {
+      $timeout(function() {
+        if($scope.select === 0 && $scope.search === "" && $scope.filterMembers.length) {
+          $scope.memberStates = getStatesByMembers($scope.members);
+          $scope.select = 0;
+        } else if($scope.filterMembers.length) {
+          $scope.memberStates = getStatesByMembers($scope.filterMembers);
+        }
+
+        $scope.paginate();
+      }, 10);
     }
 
-    $scope.pageDown = function() {
-      if($scope.currentPage > 1) {
-        $scope.currentPage--;
+    $scope.pageUp = function(newPage) {
+      if($scope.currentPage < $scope.pager.numPages) {
+        $scope.currentPage = newPage || $scope.currentPage + 1;
       }
+    };
+
+    $scope.pageDown = function(newPage) {
+      if($scope.currentPage > 1) {
+        $scope.currentPage = newPage || $scope.currentPage - 1;
+      }
+    };
+
+    $scope.clearFilters = function() {
+      $scope.select = 0;
+      $scope.search = "";
+      $scope.paginateAndFilterStates();
     }
 
   }])
@@ -79,7 +113,7 @@ angular.module('aaae')
   .directive('aaaePaginator', function() {
     return {
       restrict: 'E',
-      template: '<nav class="navbar"><ul><li><button class="button page-down" ng-click="pageDown()"> << </button></li><li><button class="button" disabled>{{currentPage}}</button></li><li><button class="button page-up" ng-click="pageUp()"> >> </button></li></ul></nav>',
+      template: '<nav class="navbar"><ul><li><button class="button page-down" ng-click="pageDown(currentPage - 1)" ng-disabled="currentPage === 1"> << </button></li><li><button class="button" disabled>{{currentPage}}</button></li><li><button class="button page-up" ng-click="pageUp(currentPage + 1)" ng-disabled="currentPage === pager.numPages"> >> </button></li></ul></nav>',
       scope: true
     };
   });
